54c0eb60f02b38f9d45804c069b9a933184489c8
easta <1210eastar@gmail.com>
2022-01-27 13:11:33 +0900

    lee

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('', include('mypageapp.urls')),
+    path('mypage/', include('mypageapp.urls')),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
