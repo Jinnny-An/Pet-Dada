bba7cb4890b80ded56d47c4f8f31222918a2b2ad
JI HEE <syos7462@daum.net>
2022-01-28 12:07:19 +0900

    jh : models와 migrations

--- main/models.py
+++ main/models.py
@@ -22,8 +22,6 @@ class Category(models.Model):
     c_id = models.AutoField(primary_key=True)
     c_name = models.CharField(max_length=50)
 
-    class Meta:
-        managed = False
     
 class Store(models.Model):
     store_id = models.AutoField(primary_key=True)
